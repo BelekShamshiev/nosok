89984e5c6df8ec5ba38566d8281f45afcd21e5bb
BelekShamshiev <zamirbekov.belek.sh@gmail.com>
2023-08-15 16:19:45 +0600

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,41 @@ import For from '@/components/For/For';
 import Header from '@/components/Header/Header';
 import Hero from '@/components/Hero/Hero';
 import Project from '@/components/Project/Project';
+import Head from 'next/head';
 import React from 'react';
 
-const index = () => {
+export default function Home() {
   return (
     <>
-      <Header/>
-      <Hero/>
-      <Project/>
-      <For/>
-      <Adversting/>
-      <About/>
-      <Card/>
-      <Contact/>
+      <Head>
+        <title>Elegant Socks</title>
+        <meta name="description" content="nosok" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Elegant Socks" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="
+          Строительство
+          Ремонт и отделка
+          Архитектура и проектирование
+          Новостройки
+          Коммерческая недвижимость
+          Жилая недвижимость
+          Промышленное строительство
+          Инженерные сети и коммуникации."
+        />
+      </Head >
+      <main>
+        <Header />
+        <Hero />
+        <Project />
+        <For />
+        <Adversting />
+        <About />
+        <Card />
+        <Contact />
+      </main>
     </>
-  );
-};
-
-export default index;
+  )
+}
